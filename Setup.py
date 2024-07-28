6251fff5a074472b430d26ae0e81b8a817ade40d
En0rooD-OR <jensaki152@gmail.com>
2024-07-28 17:34:54 -0500

    Setup.py
    Signed-off-by: En0rooD-OR <jensaki152@gmail.com>

--- Setup.py
+++ Setup.py
@@ -4,9 +4,12 @@ setup(
     name="Simple Cypher",
     description="Cesar and Vignére code cipher and decryptor"
     version="0.0.1",
-    author="En0rood",
-    pythonReq="3.12+",
-    youtube="https://www.youtube.com/@en0roodOr",
+    author="EnrooD-OR",
+    url="https://github.com/En0rooD-OR/Simple-Cypher"
+)
+
+"""
+   youtube="https://www.youtube.com/@en0roodOr",
     facebook="https://www.facebook.com/EnorooD",
     github="https://github.com/En0rooD-OR",
-)
+"""
