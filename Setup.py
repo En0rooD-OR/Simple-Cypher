90444a7bafb553c2cd4c7052c36b4eb2bab701bd
En0rooD-OR <jensaki152@gmail.com>
2024-07-28 16:01:02 -0500

    Setup.py

---
+++ Setup.py
@@ -0,0 +1,9 @@
+SetupIndex = {
+    "libName": "Simple Cypher",
+    "LibVersion": "0.0.1",
+    "Author": "Ēn0røøD",
+    "PythonReq": "3.12+",
+    "Youtube": "https://www.youtube.com/@en0roodOr",
+    "Facebook": "https://www.facebook.com/EnorooD",
+    "Github": "https://github.com/En0rooD-OR",
+}
