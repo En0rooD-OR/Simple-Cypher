8e3c5b44d3cee6fab3af3f2ec60237e14b10ae73
En0rooD-OR <jensaki152@gmail.com>
2024-07-28 17:27:44 -0500

    Setup.py
    Signed-off-by: En0rooD-OR <jensaki152@gmail.com>

--- Setup.py
+++ Setup.py
@@ -1,9 +1,12 @@
-SetupIndex = {
-    "libName": "Simple Cypher",
-    "LibVersion": "0.0.1",
-    "Author": "Ēn0røøD",
-    "PythonReq": "3.12+",
-    "Youtube": "https://www.youtube.com/@en0roodOr",
-    "Facebook": "https://www.facebook.com/EnorooD",
-    "Github": "https://github.com/En0rooD-OR",
-}
+from setuptools import setup
+
+setup(
+    name="Simple Cypher",
+    description="Cesar and Vignére code cipher and decryptor"
+    version="0.0.1",
+    author="En0rood",
+    pythonReq="3.12+",
+    youtube="https://www.youtube.com/@en0roodOr",
+    facebook="https://www.facebook.com/EnorooD",
+    github="https://github.com/En0rooD-OR",
+)
