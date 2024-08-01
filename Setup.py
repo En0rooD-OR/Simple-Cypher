ffbc47924eacb70cfc52511cc587bf088e61f192
En0rooD-OR <jensaki152@gmail.com>
2024-07-31 23:11:00 -0500

    Actualizar Setup.py

--- Setup.py
+++ Setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="Simple Cypher",
     description="Cesar and Vignére code cipher and decryptor",
-    version="0.0.1",
+    version="0.2.1",
     author="EnrooD-OR",
     author_email="jensaki152@gmail.com"
     url="https://github.com/En0rooD-OR/Simple-Cypher",
